eecbb7ae0b778a8d6cb7684149e414da3d593e8e
didierClin <didier.c62@numericable.fr>
2016-02-22 19:13:30 +0100

    added CSS in simple-todos

--- simple-todos.js
+++ simple-todos.js
@@ -2,9 +2,9 @@ if (Meteor.isClient) {
   // This code only runs on the client
   Template.body.helpers({
     tasks: [
-      { text: "This is task 1" },
-      { text: "This is task 2" },
-      { text: "This is task 3" }
+      { text: "Ceci est ma première tache" },
+      { text: "Ceci est ma seconde tache" },
+      { text: "Ceci est ma troisième tache" }
     ]
   });
 }
