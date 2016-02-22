21d59ae95af6de4441dbbe9567598c81f69a7b49
didierClin <didier.c62@numericable.fr>
2016-02-22 21:07:36 +0100

    Adding user accounts

--- simple-todos.js
+++ simple-todos.js
@@ -36,8 +36,9 @@ if (Meteor.isClient) {
       Tasks.insert({
 
         text: text,
-        createdAt: new Date() // current time
-
+        createdAt: new Date(),            // current time
+        owner: Meteor.userId(),           // _id of logged in user
+        username: Meteor.user().username  // username of logged in user
       });
 
       // Clear form
@@ -61,6 +62,10 @@ if (Meteor.isClient) {
       Tasks.remove(this._id);
     }
   });
+    
+      Accounts.ui.config({
+            passwordSignupFields: "USERNAME_ONLY"
+      });
 }
 
 
